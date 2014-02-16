db32340aa7ff86d623d4c06d124ece22cd974262
Scott Walters <srwalte2@illinois.edu>
2014-02-16 13:24:46 -0600

    Added range to hue data

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 {
 	"Synchronous (once per frame)" : {
 		"/shape" : [type (String), width (int), height (int)],
-		"/boundary" : [id (int), width (int), height (int), hue (int)]
+		"/boundary" : [id (int), width (int), height (int), hue (int [0, 360))]
 	},
 	"Asynchronous (on event)" : {
 		
