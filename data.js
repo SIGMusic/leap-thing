a77043b1b143c9c7be8f2c982cdda92baca3d08f
stpherring <stpherring@aol.com>
2014-03-02 14:07:42 -0600

    Added variables to shape OSC

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 {
 	"Synchronous (once per frame)" : {
-		"/shape" : [type (String), id (int), width (float), height (float), sides (int), x_vel (float), y_vel (float), ang_vel (float)],
+		"/shape" : [type (String), id (int), width (float), height (float), sides (int), x_pos(float), y_pos(float), x_vel (float), y_vel (float), ang_vel (float)],
 		"/boundary" : [id (int), width (float), height (float), hue (int [0, 360)), angle (float)],
 		"/hand": [id (int), x_pos (float), y_pos (float), stab_x_pos (float), stab_y_pos (float), x_dir (float), y_dir (float), roll (float), pitch (float), yaw (float), t (float), sp_x_pos (float), sp_y_pos (float), radius (float)],
 		"/finger": [hand_id (int), finger_id (int), x_pos (float), y_pos (float)],
